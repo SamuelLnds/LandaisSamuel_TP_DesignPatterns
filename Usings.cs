4a567eab4a6aa04b3da5a40f25684b258b29a547
SamuelLnds <samuel.landais.pro@gmail.com>
2026-03-03 09:57:42 +0100

    Ajout des DP qui sont obligatoires à faire pour la fin du TP

--- Usings.cs
+++ Usings.cs
@@ -1,3 +1,5 @@
+// Global using permet d'éviter de répéter les directives using dans chaque fichier du projet
+// La déclaration des usings dans un seul fichier centralisé rend Program.cs plus propre
 global using LandaisSamuel_TP_DesignPatterns.Adapter;
 global using LandaisSamuel_TP_DesignPatterns.Command;
 global using LandaisSamuel_TP_DesignPatterns.Decorator;
